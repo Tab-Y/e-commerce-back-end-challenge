58a711a08c1db7c350cbb5e7bc7c6f1d28701e65
Tab-Y <tlyoumans@gmail.com>
2022-11-07 21:09:51 -0500

    models set per readme guidelines

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE"
+});
 
 // Categories have many Products
+Category.hasMany(Products, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+
+  },
+
+})
 
 // Tags belongToMany Products (through ProductTag)
+TimeRanges.belongsToMany(Products, {
+  through: {
+    model: ProductTag,
+
+  },
+  
+})
 
 module.exports = {
   Product,
