49bceab0085303d20825eec26f16aa465879973c
Tab-Y <tlyoumans@gmail.com>
2022-11-07 23:05:37 -0500

    fixed table assosiation issue

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,21 +14,32 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {id: req.params.id}
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id. Please check your input and try again.'})
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
