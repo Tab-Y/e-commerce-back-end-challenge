b0c15086d96f9dc2a2b76c1230a066f7f091e537
Tab-Y <tlyoumans@gmail.com>
2022-11-07 23:42:07 -0500

    post route + working through update route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,10 +34,30 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {id: req.params.id}
+    })
+    .then((category) => {
+      return
+    })
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id. Please check your input and try again.'})
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
