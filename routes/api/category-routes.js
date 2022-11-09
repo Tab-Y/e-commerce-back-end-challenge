ad3bfb316639f60f93d11aae5e855156a48fc4ff
Tab-Y <tlyoumans@gmail.com>
2022-11-08 19:48:57 -0500

    fixxed decimal issue not showing, routes on product and tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,9 @@ router.get('/', async (req, res) => {   // working
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]      // includes associated products
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -19,7 +21,7 @@ router.get('/:id', async (req, res) => {    // working
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      
+      include: [{ model: Product }]    // includes associated products
     });
 
     if (!categoryData) {
