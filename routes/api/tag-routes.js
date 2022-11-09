867018c1529c857d8d3b68fd958e1b6bf07de245
Tab-Y <tlyoumans@gmail.com>
2022-11-08 19:55:44 -0500

    full functionality

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, through: ProductTag }]      // includes associated products
+      include: [{ model: Product}]      // includes associated products
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag }]    // includes associated products
+      include: [{ model: Product}]    // includes associated products
     });
 
     if (!tagData) {
